7907968695fdcd64ea70adaee70f7361f5773f35
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-02-09 17:32:23 +0100

    feat: use @ziqquratu/database 1.0.0

--- src/collection.ts
+++ src/collection.ts
@@ -1,62 +1,117 @@
-import {Collection, QueryOptions} from '@ziqquratu/database';
+import {
+  Collection,
+  Cursor,
+  ReplaceOneOptions,
+  SortingDirection,
+  SortingKey,
+  sortingMap,
+  applyQueryOptions,
+  QueryOptions
+} from '@ziqquratu/database';
 import {EventEmitter} from 'eventemitter3';
-import {Collection as MongoCollection} from 'mongodb';
+import * as mongo from 'mongodb';
+
+export class MongoDBCursor<T = any> implements Cursor<T> {
+  public constructor(
+    private cursor: mongo.Cursor,
+    options: QueryOptions = {},
+  ) {
+    applyQueryOptions(this, options);
+  }
+
+  public sort(key: SortingKey, direction?: SortingDirection): Cursor<T> {
+    this.cursor.sort(sortingMap(key, direction));
+    return this;
+  }
+
+  public skip(count: number): Cursor<T> {
+    this.cursor.skip(count);
+    return this;
+  }
+
+  public limit(count: number): Cursor<T> {
+    this.cursor.limit(count);
+    return this;
+  }
+
+  public async next(): Promise<T | null> {
+    return this.cursor.next();
+  }
+
+  public async hasNext(): Promise<boolean> {
+    return this.cursor.hasNext();
+  }
+
+  public async forEach(iterator: (doc: T) => void): Promise<void> {
+    return this.cursor.forEach(iterator);
+  }
+
+  public async toArray(): Promise<T[]> {
+    return this.cursor.toArray();
+  }
+
+  public async count(applySkipLimit = true): Promise<number> {
+    return this.cursor.count(applySkipLimit);
+  }
+}
 
 export class MongoDBCollection extends EventEmitter implements Collection {
   public constructor(
-    private collection: MongoCollection,
+    private collection: mongo.Collection,
     public readonly name: string
   ) {
     super();
   }
 
-  public async find(selector: object = {}, options: QueryOptions = {}): Promise<any[]> {
-    let cursor = this.collection.find(selector);
-    if (options.sort) {
-      cursor = cursor.sort(options.sort);
-    }
-    if (options.offset) {
-      cursor = cursor.skip(options.offset);
-    }
-    if (options.limit) {
-      cursor = cursor.limit(options.limit);
-    }
-    return cursor.toArray();
+  public find(selector: object = {}, options?: QueryOptions): Cursor<any> {
+    return new MongoDBCursor(this.collection.find(selector), options);
   }
 
   public async findOne(selector: object): Promise<any> {
-    const doc = await this.collection.findOne(selector);
-    if (!doc) {
-      throw new Error('Failed to find document in collection');
-    }
-    return doc;
+    return this.collection.findOne(selector);
   }
 
-  public async upsert(doc: any): Promise<any> {
-    let res: any;
-    try {
-      res = await this.collection.insertOne(doc);
-    } catch (err) {
-      res = await this.collection.updateOne({_id: doc._id}, {$set: doc});
-    }
+  public async insertOne(doc: any): Promise<any> {
+    let res = await this.collection.insertOne(doc);
     if (res.result.ok !== 1) {
-      throw Error('Failed to upsert document');
+      throw Error('Failed to insert document');
     }
     const resultDoc = res.ops ? res.ops[0] : doc;
     this.emit('document-upserted', resultDoc);
     return resultDoc;
   }
 
-  public async remove(selector: object): Promise<any[]> {
-    const docs = await this.find(selector);
+  public async insertMany(docs: any[]): Promise<any[]> {
+    let res = await this.collection.insertMany(docs);
+    if (res.result.ok !== 1) {
+      throw Error('Failed to insert documents');
+    }
+    const resultDocs = res.ops ? res.ops : docs;
+    for (const doc of resultDocs) {
+      this.emit('document-upserted', doc);
+    }
+    return resultDocs;
+  }
+
+  public async replaceOne(selector: object, doc: any, options?: ReplaceOneOptions): Promise<any> {
+    return this.collection.replaceOne(selector, doc, options);
+  }
+
+  public async deleteOne(selector: object): Promise<any> {
+    const doc = await this.findOne(selector);
+    const res = await this.collection.deleteOne(selector);
+    if (doc && res.deletedCount === 1) {
+      this.emit('document-removed', doc);
+    }
+    return doc;
+  }
+
+  public async deleteMany(selector: object): Promise<any[]> {
+    const docs = await this.find(selector).toArray();
     await this.collection.deleteMany(selector);
     for (let doc of docs) {
       this.emit('document-removed', doc);
     }
     return docs;
-  }
-
-  public async count(selector?: object): Promise<number> {
-    return this.collection.countDocuments(selector);
   }
 }
--- test/collection.spec.ts
+++ test/collection.spec.ts
@@ -31,111 +31,161 @@ describe('MongoDBCollection', () => {
   });
 
   beforeEach(async () => {
-    await col.upsert({_id: 1, item: { category: 'cake', type: 'chiffon' }, amount: 10 });
-    await col.upsert({_id: 2, item: { category: 'cookies', type: 'chocolate chip'}, amount: 50 });
-    await col.upsert({_id: 3, item: { category: 'cookies', type: 'chocolate chip'}, amount: 15 });
-    await col.upsert({_id: 4, item: { category: 'cake', type: 'lemon' }, amount: 30 });
-    await col.upsert({_id: 5, item: { category: 'cake', type: 'carrot' }, amount: 20 });
+    await col.insertMany([
+      {_id: 1, item: { category: 'cake', type: 'chiffon' }, amount: 10 },
+      {_id: 2, item: { category: 'cookies', type: 'chocolate chip'}, amount: 50 },
+      {_id: 3, item: { category: 'cookies', type: 'chocolate chip'}, amount: 15 },
+      {_id: 4, item: { category: 'cake', type: 'lemon' }, amount: 30 },
+      {_id: 5, item: { category: 'cake', type: 'carrot' }, amount: 20 },
+    ]);
   });
 
   afterEach(async () => {
     col.removeAllListeners();
-    await col.remove({});
+    await col.deleteMany({});
   });
 
-  describe('upsert', () => {
+  describe('insertOne', () => {
     it('should add a single document and give it an id', async () => {
-      const doc = await col.upsert(
+      const doc = await col.insertOne(
         {item: { category: 'brownies', type: 'blondie' }, amount: 10 }
       );
       expect(doc.amount).to.eql(10);
       expect(doc).to.haveOwnProperty('_id');
     });
-    it('should update a single document', async () => {
-      const doc1: any = await col.upsert(
-        {item: { category: 'brownies', type: 'blondie' }, amount: 10 }
-      );
-      const doc2 = await col.upsert(
-        {_id: doc1._id, item: { category: 'brownies', type: 'blondie' }, amount: 20 }
-      );
-      expect(doc1._id).to.eql(doc2._id);
-      expect(doc1.amount).to.eql(10);
-      expect(doc2.amount).to.eql(20);
+    it('should throw when trying to insert a document with already existing ID', () => {
+      return expect(col.insertOne(
+        {_id: 1, item: { category: 'brownies', type: 'blondie' }, amount: 10 }
+      )).to.eventually.be.rejected;
     });
-    it('should emit a document-upserted event', async () => {
-      let spy = sinon.spy();
-      col.on('document-upserted', spy);
-      await col.upsert({_id: 'foo'});
-      expect(spy).to.have.been.calledWith({_id: 'foo'});
+  });
+
+  describe('insertMany', () => {
+    it('should add a multiple documents and give them ids', async () => {
+      const docs = await col.insertMany([
+        {item: { category: 'brownies', type: 'blondie' }, amount: 10 },
+        {item: { category: 'brownies', type: 'baked' }, amount: 12 },
+      ]);
+      expect(docs.length).to.eql(2);
+      expect(docs[0].amount).to.eql(10);
+      expect(docs[1].amount).to.eql(12);
+      expect(docs[0]).to.haveOwnProperty('_id');
+      expect(docs[1]).to.haveOwnProperty('_id');
+    });
+    it('should throw when trying to insert a document with already existing ID', () => {
+      return expect(col.insertMany([
+        {item: { category: 'brownies', type: 'blondie' }, amount: 10 },
+        {_id: 1, item: { category: 'brownies', type: 'baked' }, amount: 12 },
+      ])).to.eventually.be.rejected;
+    });
+  });
+
+  describe.skip('replaceOne', () => {
+    it('should update a single document', async () => {
+      const doc = await col.replaceOne(
+        {_id: 1}, {item: { category: 'brownies', type: 'blondie' }, amount: 20 }
+      );
+      expect(doc._id).to.eql(1);
+      expect(doc.amount).to.eql(20);
+    });
+    it('should return null if no document matched selector', async () => {
+      const doc = await col.replaceOne(
+        {_id: 6}, {item: { category: 'brownies', type: 'blondie' }, amount: 20 }
+      );
+      expect(doc).to.eql(null);
+    });
+    it('should completely replace document', async () => {
+      const doc = await col.replaceOne(
+        {_id: 1}, { amount: 20 }
+      );
+      expect(doc.item).to.eql(undefined);
+    });
+    it('should upsert when specified', async () => {
+      const doc = await col.replaceOne(
+        {_id: 6}, { amount: 20 }, {upsert: true}
+      );
+      expect(doc.amount).to.eql(20);
     });
   });
 
   describe('count', () => {
     it('should return 0 when no documents are matching', () => {
-      expect(col.count({'item.category': 'candy'})).to.eventually.eql(0);
+      expect(col.find({'item.category': 'candy'}).count()).to.eventually.eql(0);
     });
     it('should be a positive number when items are matched', async () => {
-      expect(col.count({'item.category': 'cake'})).to.eventually.eql(3);
+      expect(col.find({'item.category': 'cake'}).count()).to.eventually.eql(3);
     });
   });
 
   describe('findOne', () => {
-    it('should throw when document is not found', () => {
-      return expect(col.findOne({_id: 7})).to.eventually.be.rejectedWith('');
+    it('should return null when document is not found', () => {
+      return expect(col.findOne({_id: 7})).to.eventually.eql(null);
     });
     it('should return the document when found', async () => {
-      let doc = await col.findOne({_id: 1});
+      const doc = await col.findOne({_id: 1});
       expect(doc).to.haveOwnProperty('amount').equals(10);
     });
   });
 
   describe('find', () => {
     it('should return empty list when no documents match selector', () => {
-      return expect(col.find({_id: 7})).to.eventually.be.empty;
+      return expect(col.find({_id: 7}).toArray()).to.eventually.be.empty;
     });
     it('should return a list of matched documents', async () => {
-      const docs = await col.find({'item.type': 'chiffon'});
+      const docs = await col.find({'item.type': 'chiffon'}).toArray();
       expect(docs).to.have.length(1);
       expect(docs[0]).to.haveOwnProperty('_id').equal(1);
     });
     it('should handle query operators', async () => {
-      const docs = await col.find({_id: {$in: [1, 2, 7]}});
+      const docs = await col.find({_id: {$in: [1, 2, 7]}}).toArray();
       expect(docs).to.have.length(2);
     });
-    it('should do sorting', async () => {
-      const docs = await col.find({}, {sort: {'item.category': 1, 'item.type': 1}});
+    it('should do sorting with key', async () => {
+      const docs = await col.find().sort('amount', 1).toArray();
+      expect(docs[0].item.type).to.eql('chiffon');
+    });
+    it('should do sorting with map', async () => {
+      const docs = await col.find().sort({'item.category': 1, 'item.type': 1}).toArray();
+      expect(docs[0].item.type).to.eql('carrot');
+    });
+    it('should do sorting with array', async () => {
+      const docs = await col.find().sort(['item.category', 'item.type'], 1).toArray();
       expect(docs[0].item.type).to.eql('carrot');
     });
     it('should do offset and limiting', async () => {
-      const docs = await col.find({}, {
-        sort: {'amount': -1},
-        offset: 1,
-        limit: 1
-      });
+      const docs = await col.find().sort('amount', -1).skip(1).limit(1).toArray();
       expect(docs).to.have.length(1);
       expect(docs[0].item.type).to.eql('lemon');
+    });
+    it('should accept query options', async () => {
+      const docs = await col.find({}, {sort: {amount: -1}, skip: 1, limit: 1}).toArray();
+      expect(docs).to.have.length(1);
+      expect(docs[0].item.type).to.eql('lemon');
+    });
+    it.skip('should be able to iterate', async () => {
+      const cursor = col.find().sort('amount', -1).skip(1).limit(1);
+      expect(await cursor.hasNext()).to.be.true;
+      expect((await cursor.next()).item.type).to.eql('lemon');
+      expect(await cursor.hasNext()).to.be.false;
+      expect(await cursor.next()).to.eql(null);
     });
   });
 
   describe('remove', () => {
     it('should return empty list when no documents match selector', () => {
-      return expect(col.remove({_id: 7})).to.eventually.be.empty;
-    });
-    it('should not emit event when no documents were removed', async () => {
-      let spy = sinon.spy();
-      col.on('document-removed', spy);
-      await col.remove({_id: 7});
-      expect(spy).to.have.callCount(0);
+      return expect(col.deleteMany({_id: 7})).to.eventually.be.empty;
     });
     it('should return a list of deleted documents', async () => {
-      const docs = await col.remove({'item.category': 'cookies'});
+      const docs = await col.deleteMany({'item.category': 'cookies'});
       expect(docs).to.have.length(2);
     });
-    it('should emit events when documents were removed', async () => {
-      let spy = sinon.spy();
-      col.on('document-removed', spy);
-      await col.remove({'item.category': 'cookies'});
-      expect(spy).to.have.callCount(2);
+    it('should have removed selected documents', async () => {
+      await col.deleteMany({'item.category': 'cookies'});
+      return expect(col.find({'item.category': 'cookies'}).count()).to.eventually.eql(0);
+    });
+    it('should not remove other documents', async () => {
+      await col.deleteMany({'item.category': 'cookies'});
+      return expect(col.find().count()).to.eventually.eql(3);
     });
   });
 });
